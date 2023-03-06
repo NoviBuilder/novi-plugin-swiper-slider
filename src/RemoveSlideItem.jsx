9389e09d5048e86b11e8c4bf7847b2e6afbea6ed
Léo MARCEL <leo.marcel.pro@gmail.com>
2023-03-06 15:41:41 +0100

    Update RemoveSlideItem.jsx

--- src/RemoveSlideItem.jsx
+++ src/RemoveSlideItem.jsx
@@ -22,7 +22,7 @@ function removeSlide(element) {
     let slidesCount = Utils.getSlidesCount(element);
     if (!currentSlide || slidesCount <= 1) return;
 
-    // When remove a slide, the next slide doesn't display the content because the animation ddoesn't work
+    // When remove a slide, the next slide doesn't display the content because the animation doesn't work
     Utils.removeAnimationWhenRemoveSlide(element, 
         currentSlideIndex != slidesCount -1 ? currentSlideIndex+1 : currentSlideIndex-1);
 
